cd3b0549944c8993b4b685a15c33539002ba6fd6
shuya goto <f20065@g.ichinoseki.ac.jp>
2023-11-28 04:22:15 +0900

    一箇所からexport

---
+++ src/components/common/flexBox/index.ts
@@ -0,0 +1,7 @@
+import { FlexBox } from './flexBox';
+import { Text } from '../text/text';
+import { TextBox, TextBoxProps } from './textBox/textBox';
+
+export { FlexBox, Text, TextBox };
+export type { TextBoxProps };
+
