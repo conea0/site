bd3af2b53e6264f3e535601aca2dba8f1f79c559
shuya goto <f20065@g.ichinoseki.ac.jp>
2023-11-28 04:21:54 +0900

    シンプルなテキストコンポーネントを定義

---
+++ src/components/common/text/text.tsx
@@ -0,0 +1,24 @@
+import styles from "./text.module.scss";
+
+interface TextProps {
+  title: string;
+  text: string;
+  width: 'full' | 'half'
+}
+
+export const Text = ({
+  title,
+  text,
+  width,
+  children,
+}: React.PropsWithChildren<TextProps>) => {
+  return (
+    <div className={`${styles.section} ${width == "full" ? "" : styles.half}`}>
+      <div className={`${styles.container} ${width == "full" ? "" : styles.halfContainer}`}>
+        <h2>{title}</h2>
+        <p>{text}</p>
+        <div className={styles.children}>{children}</div>
+      </div>
+    </div>
+  );
+};
